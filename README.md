4e4a028a41343a385fc672190fc30c0a2915cacb
adlard07 <adelarddcunha@gmail.com>
2023-12-08 11:54:08 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Web-Scraping
+
+## Overview
+
+This project is a Python script that demonstrates web scraping dynamic websites using Selenium and BeautifulSoup using python and illustrates how to interact with DuckDB, a database management system, using the DuckDB Python library and finally saving it locally in CSV format. 
+
+
+## Install dependencies:
+
+```bash
+pip install -r requirements.txt
+```
+
+## Run Program
+
+```bash
+python -m database
+```
--- database.py
+++ database.py
@@ -6,21 +6,21 @@ import time
 
 conn = duckdb.connect(database=':memory:', read_only=False)
 
-if os.path.exists('mydb.db'):
-    result = conn.execute("SELECT * FROM my_table").fetchdf()
-    
-else:
-    driver = webdriver.Edge()
-    url = "https://ocimpact.app.swapcard.com/widget/event/oc2023/people/RXZlbnRWaWV3XzQ1NTQwOA==?showActions=true"
-    time.sleep(3)
-    profiles = Profiles()
-    driverr, links = profiles.get_profiles(url, driver)
-    output = profiles.json_output(driver, links[:5])
-    
-    schema_definition = ', '.join([f'"{col}" VARCHAR(30)' for col in output.columns])
-    conn.execute(f"CREATE TABLE my_table ({schema_definition})")
+driver = webdriver.Edge()
+url = "https://ocimpact.app.swapcard.com/widget/event/oc2023/people/RXZlbnRWaWV3XzQ1NTQwOA==?showActions=true"
+time.sleep(3)
+profiles = Profiles()
+driverr, links = profiles.get_profiles(url, driver)
+output = profiles.json_output(driver, links[:5])
 
-    conn.register('my_table', output)
-    result = conn.execute("SELECT * FROM my_table").fetchdf()
+schema_definition = ', '.join([f'"{col}" VARCHAR(30)' for col in output.columns])
+conn.execute(f"CREATE TABLE my_table ({schema_definition})")
 
+conn.register('my_table', output)
+result = conn.execute("SELECT * FROM my_table").fetchdf()
+
+path = 'mytable.csv'
+output.to_csv(path, index=False)
+
+conn.close()
 print(result)
